5eae6eaa250a035e577870ed1bc8596023861bf0
davidzhukovsky <zhukovsky.david@gmail.com>
2022-04-11 08:57:52 -0400

    4/11/2022 - UI Updates, Settings Button Functionality implemented

---
+++ app/src/main/java/com/example/a4g_ksd/SettingsProfile.java
@@ -0,0 +1,11 @@
+package com.example.a4g_ksd;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SettingsProfile extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.settings_profile);
+}}
