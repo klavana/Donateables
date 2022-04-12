db2dcbd4eabbad3437223cba76aa67863e918508
davidzhukovsky <zhukovsky.david@gmail.com>
2022-04-11 23:52:03 -0400

    4/11/2022 - Settings page, User Special Instructions implementation, spelling errors fixed, UI updates, misc touch-ups :) !

--- app/src/main/java/com/example/a4g_ksd/SettingsProfile.java
+++ app/src/main/java/com/example/a4g_ksd/SettingsProfile.java
@@ -1,6 +1,9 @@
 package com.example.a4g_ksd;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -8,4 +11,14 @@ public class SettingsProfile extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.settings_profile);
-}}
+        TextView email = findViewById(R.id.textView3);
+        TextView addy = findViewById(R.id.textView6);
+        TextView instr = findViewById(R.id.textView8);
+        Intent intent = getIntent();
+        final Controller controller = (Controller) getApplicationContext();
+        addy.setText(" " + controller.getUser().getAddress());
+        email.setText(" " + controller.getUser().getEmail());
+        instr.setText(" " + controller.getUser().getInstr());
+    }
+
+}
