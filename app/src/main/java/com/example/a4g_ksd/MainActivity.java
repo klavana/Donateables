dbe152bc867ce61351bd991284b57f6b6aa27a81
karismalavana <klavana@wpi.edu>
2022-04-27 08:11:36 -0400

    merged with local master, fixed any errors, push attempt

--- app/src/main/java/com/example/a4g_ksd/MainActivity.java
+++ app/src/main/java/com/example/a4g_ksd/MainActivity.java
@@ -103,9 +103,35 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void moveToMainPage(View v) {
-        Intent intent = new Intent(this, MainPage.class);
-        startActivity(intent);
+        if(boolin) {
+            setUser(v);
+            Intent intent = new Intent(this, MainPage.class);
+            startActivity(intent);
+        }
+        else {
+            setUser(v);
+            Intent intent = new Intent(this, MainOrgPage.class);
+            startActivity(intent);
+        }
     }
+
+    public void onRadioButtonClicked(View view) {
+        // Is the button now checked?
+        boolean checked = ((RadioButton) view).isChecked();
+
+        // Check which radio button was clicked
+        switch(view.getId()) {
+            case R.id.genUserRadio:
+                if (checked)
+                    boolin = true;
+                break;
+            case R.id.orgRadio:
+                if (checked)
+                    boolin = false;
+                break;
+        }
+    }
+
 
 
 }
--- app/src/main/java/com/example/a4g_ksd/MainPage.java
+++ app/src/main/java/com/example/a4g_ksd/MainPage.java
@@ -44,9 +44,4 @@ public class MainPage extends AppCompatActivity {
         intent.putExtra("key3", CompOffer);
         startActivity(intent);
     }
-
-    public void moveToSettings(View v) {
-        Intent intent = new Intent(this, SettingsProfile.class);
-        startActivity(intent);
-    }
 }
